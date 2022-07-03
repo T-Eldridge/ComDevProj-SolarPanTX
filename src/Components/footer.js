403f2bfc517a335dbd470f2518ca3bbd7eb65014
TEldridge <skippydrumskins@yahoo.com>
2022-07-03 13:57:29 -0500

    added insta logo, still broken paths

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -2,10 +2,10 @@ import React from "react";
 import './footer.css';
 
 const Footer = () => <div className="Footer">
-    <a href="https://www.bbb.org/"><img src="../../public/images/BBB-Logo.jpg" alt="BBB" /></a>
+    <a href="https://www.bbb.org/"><img src="../public/images/BBB-Logo.jpg" alt="BBB" /></a>
     <a href="https://www.youtube.com/"><img  src="../../public/images/yt-thumb.jpg" alt="YT"/></a>
     <a href="https://www.facebook.com/"><img  src="images/facebook.jpg" alt="FB"/></a>
-    
+    <a href="https://www.instagram.com/?hl=en"><img  src="../images/2048px-Instagram_icon.png" alt="IG"/></a>
 </div>
 
 
