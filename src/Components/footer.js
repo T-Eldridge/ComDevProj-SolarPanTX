fea7dad618b5e6bd29a9ec7b53c1a38db6773549
TEldridge <skippydrumskins@yahoo.com>
2022-07-03 11:19:09 -0500

    fixing images still

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -2,8 +2,8 @@ import React from "react";
 import './footer.css';
 
 const Footer = () => <div className="Footer">
-    <a href="https://www.bbb.org/"><img src="/images/BBB-Logo.jpg" alt="BBB" /></a>
-    <a href="https://www.youtube.com/"><img  src="images/yt-thumb.jpg" alt="YT"/></a>
+    <a href="https://www.bbb.org/"><img src="../../public/images/BBB-Logo.jpg" alt="BBB" /></a>
+    <a href="https://www.youtube.com/"><img  src="../../public/images/yt-thumb.jpg" alt="YT"/></a>
     <a href="https://www.facebook.com/"><img  src="images/facebook.jpg" alt="FB"/></a>
     
 </div>
