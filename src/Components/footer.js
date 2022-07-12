16818d206a1f98ab35afc8f2167f5f606d501bca
TEldridge <skippydrumskins@yahoo.com>
2022-07-12 10:59:00 -0500

    Added twitter icon and fixed anchors to correct links

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,13 +4,15 @@ import Logo from "../images/BBB-Logo.jpg"
 import YT from "../images/yt-thumb.jpg";
 import FB from "../images/facebook.jpg";
 import IG from "../images/2048px-Instagram_icon.png";
+import Twitter from "../images/Twitter-logo.svg.png";   
 
 
 const Footer = () => <div className="Footer">
     <a href="https://www.bbb.org/"><img src={Logo} alt="BBB" /></a>
-    <a href="https://www.youtube.com/"><img  src={YT} alt="YT"/></a>
-    <a href="https://www.facebook.com/"><img  src={FB} alt="FB"/></a>
-    <a href="https://www.instagram.com/?hl=en"><img  src={IG} alt="IG"/></a>
+    <a href="https://www.youtube.com/channel/UCs6__DhtjJ_olrruo9flyLQ"><img  src={YT} alt="YT"/></a>
+    <a href="https://www.facebook.com/profile.php?id=100078358163935"><img  src={FB} alt="FB"/></a>
+    <a href="https://www.instagram.com/sptxservices/"><img  src={IG} alt="IG"/></a>
+    <a href="https://twitter.com/Sptxservices"><img src={Twitter} alt="Twitter"/></a>
 </div>
 
 
